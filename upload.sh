64236d83608f90166ea07e22449acd5a9e95c610
Ewheeler121 <ewheeler121@outlook.com>
2024-04-24 11:21:18 -0400

    small bug fix to rsync

--- upload.sh
+++ upload.sh
@@ -4,5 +4,5 @@ cargo build --release
 
 rsync -avz --delete -e "ssh -i server_key.pem" target/release/website admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
 rsync -avz --delete -e "ssh -i server_key.pem" run.sh admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
-rsync -avz --delete -e "ssh -i server_key.pem" static/* admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
-rsync -avz --delete -e "ssh -i server_key.pem" entries/* admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
+rsync -ravz --delete -e "ssh -i server_key.pem" static admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
+rsync -ravz --delete -e "ssh -i server_key.pem" entries admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
