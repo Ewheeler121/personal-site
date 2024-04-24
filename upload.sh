02d4c2032017ed967cee62970a426381bb3b91e7
Ewheeler121 <ewheeler121@outlook.com>
2024-04-24 11:16:47 -0400

    added another entry

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 cargo build --release
+
 rsync -avz --delete -e "ssh -i server_key.pem" target/release/website admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
 rsync -avz --delete -e "ssh -i server_key.pem" run.sh admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
 rsync -avz --delete -e "ssh -i server_key.pem" static/* admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
