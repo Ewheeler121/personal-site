34d5f60b4631ec92f456345cbd6042ff05e8fd3c
Ewheeler121 <ewheeler121@outlook.com>
2024-04-24 10:46:41 -0400

    remove some scripts

--- connect-ssh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kitty +kitten ssh -i "server_key.pem" admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com
--- upload-program.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cargo build --release
-scp -i "server_key.pem" target/release/website admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~
-scp -i "server_key.pem" run.sh admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~
