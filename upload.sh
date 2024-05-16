629b0df3bea6e39728a4c3d37c9b9cab65d627c3
Ewheeler121 <ewheeler121@outlook.com>
2024-05-16 17:24:45 -0400

    made connect.sh more universal

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 cargo build --release
 
-rsync -avz --delete -e "ssh -i server_key.pem" target/release/website admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
-rsync -avz --delete -e "ssh -i server_key.pem" run.sh admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
-rsync -ravz --delete -e "ssh -i server_key.pem" static admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
-rsync -ravz --delete -e "ssh -i server_key.pem" entries admin@ec2-3-19-120-57.us-east-2.compute.amazonaws.com:~ 
+rsync -avz --delete -e "ssh -i server_key.pem" target/release/website admin@unpaidrust.dev:~ 
+rsync -avz --delete -e "ssh -i server_key.pem" run.sh admin@unpaidrust.dev:~ 
+rsync -ravz --delete -e "ssh -i server_key.pem" static admin@unpaidrust.dev:~ 
+rsync -ravz --delete -e "ssh -i server_key.pem" entries admin@unpaidrust.dev:~ 
